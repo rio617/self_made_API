68fffefbc9e2f452cb7760852ff0f90aa0696860
rio <rt617617@gmail.com>
2023-10-17 21:37:14 +0900

    update1017

--- main.py
+++ main.py
@@ -1,37 +1,100 @@
 import tkinter as tk
-from PIL import Image,ImageTk
+from tkinter import filedialog
+from PIL import Image, ImageTk
+import mosaic
 
-root=tk.Tk()
-root.geometry("650x650")
+# 必要な外部モジュール
+# Pillow
+# opencv-python
+
+
+# 画像選択
+def image_select():
+    img_path = filedialog.askopenfilename(
+        title="画像を選択",
+        initialdir="\Downloads",
+        filetypes=[("Image File", "*.png;*.jpg")],
+    )
+
+    mosaic.select_img = img_path
+    change_image(img_path)
+
+
+# 画像読み込み・画像変更
+def change_image(img_path):
+    global label
+    # Pillowで画像読み込み
+    pil_img = Image.open(img_path)
+    w = pil_img.width  # 画像の横幅を取得
+    h = pil_img.height  # 画像の縦幅を取得
+    # 読み込んだ画像を半分にする
+    pil_img = pil_img.resize((int(w / 2), int(h / 2)))
+    # Pillowで読み込んだ画像をtkinterで使えるように変換
+    tk_img = ImageTk.PhotoImage(pil_img)
+    # ラベルウィジェットの画像変更
+    label.configure(image=tk_img)
+    label.image = tk_img
+
+
+# 顔　モザイク処理後　画面に出す
+def push_face_mosaic():
+    mosaic_path = mosaic.face_mosaic()
+    print(mosaic_path)
+    change_image(mosaic_path)
+
+
+# 目　モザイク処理後　画面に出す
+def push_eye_mosaic():
+    mosaic_path = mosaic.eye_mosaic()
+    print(mosaic_path)
+    change_image(mosaic_path)
+
+
+# 笑顔　モザイク処理後　画面に出す
+def push_smile_mosaic():
+    mosaic_path = mosaic.smile_mosaic()
+    print(mosaic_path)
+    change_image(mosaic_path)
+
+
+# tkinterでウィンドウ用意
+root = tk.Tk()
+root.geometry("750x750")
+
+title = tk.Label(text="画像にモザイクをかけるアプリ", font=60)
+title.pack()
 
 # フレーム作成
-entry_frame=tk.Frame(root)
-button_frame=tk.Frame(root)
+entry_frame = tk.Frame(root)
+button_frame = tk.Frame(root)
+image_frame = tk.Frame(root)
+image_frame.pack()
+
 
 # ファイル選択欄
-entry=tk.Entry(entry_frame)
-entry_button=tk.Button(entry_frame,text="ボタン")
+entry = tk.Entry(entry_frame)
+entry_button = tk.Button(entry_frame, text="画像を選択", command=image_select)
 
 # 画面表示
 entry_frame.pack()
-entry.grid(row=0,column=0)
-entry_button.grid(row=0,column=1)
+entry.grid(row=0, column=0)
+entry_button.grid(row=0, column=1)
 
 # 各種ボタン
-face_button=tk.Button(button_frame,text="顔",font=30)
-eye_button=tk.Button(button_frame,text="目",font=30)
-smile_button=tk.Button(button_frame,text="笑顔",font=30)
+face_button = tk.Button(button_frame, text="顔をモザイク", font=30, command=push_face_mosaic)
+eye_button = tk.Button(button_frame, text="瞳をモザイク", font=30, command=push_eye_mosaic)
+smile_button = tk.Button(
+    button_frame, text="笑顔をモザイク", font=30, command=push_smile_mosaic
+)
 
 # 画面表示
-button_frame.pack()
-face_button.grid(row=0,column=0)
-eye_button.grid(row=0,column=1)
-smile_button.grid(row=0,column=2)
+button_frame.pack(pady=20)
+face_button.grid(row=0, column=0)
+eye_button.grid(row=0, column=1)
+smile_button.grid(row=0, column=2)
 
-# 1. Pillowで画像読み込み**
-# 2. Pillowで読み込んだ画像をtkinterで使えるように変換**
-pil_img = Image.open("image.png")
-tk_img = ImageTk.PhotoImage(pil_img)
-
+# tkinterでラベル用意、画面表示
+label = tk.Label(image_frame)
+label.pack()
 
 root.mainloop()
